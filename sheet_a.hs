dffd59f28fb5f2db67789d45c4ae8cbbe7b6e2ad
db5n17 <db5n17@soton.ac.uk>
2019-10-22 00:14:10 +0100

    Completed A1 and A2.

--- sheet_a.hs
+++ sheet_a.hs
@@ -1,4 +1,18 @@
 -- Exercise A1
+histogram' :: Int -> [Int] -> [Int]
+histogram' n [] = []
+histogram' n (x:xs) = [x `div` n] ++ histogram' n xs
+
+count :: Int -> [Int] -> Int
+count n xs = length [ x | x <- xs, x == n ]
+
 histogram :: Int -> [Int] -> [Int]
-histogram n xs | xs == []  = []
-               | otherwise = [ x | x <- xs, n-n >= x >= n ]
+histogram n xs = [ count x (divlist) | x <- [0..maximum(divlist)] ] where divlist = histogram' n xs
+
+-- Exercise A2
+approxSqrt' :: Double -> Double -> Double -> Double
+approxSqrt' n d epsilon | abs (n - sqrt d) < epsilon = n
+                        | otherwise                  = approxSqrt' ((n + d / n) / 2) d epsilon
+
+approxSqrt :: Double -> Double -> Double
+approxSqrt d epsilon = approxSqrt' 1 d epsilon

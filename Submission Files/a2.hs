2d59d5a276dbc5034585cdbd0eae86e013cb4b06
db5n17 <db5n17@soton.ac.uk>
2019-10-22 00:48:14 +0100

    Modified files accordingly for submission purposes.

--- Submission Files/a2.hs
+++ Submission Files/a2.hs
@@ -1,14 +1,14 @@
--- Exercise A1
-histogram' :: Int -> [Int] -> [Int]
-histogram' n [] = []
-histogram' n (x:xs) = [x `div` n] ++ histogram' n xs
+--TEMPLATE FILE FOR COURSEWORK 1 for COMP2209
+--Julian Rathke, Oct 2019
 
-count :: Int -> [Int] -> Int
-count n xs = length [ x | x <- xs, x == n ]
+--EXERCISE A2 ONLY
 
-histogram :: Int -> [Int] -> [Int]
-histogram n xs | n <= 0    = error "n cannot be negative or equal to zero!" 
-               | otherwise = [ count x (divlist) | x <- [0..maximum(divlist)] ] where divlist = histogram' n xs
+--CONTAINS FUNCTION REQIURED FOR COMPILATION AGAINST THE TEST SUITE
+--MODIFY THE FUNCTION DEFINITIONS WITH YOUR OWN SOLUTIONS
+--IMPORTANT : DO NOT MODIFY ANY FUNCTION TYPES
+
+
+module Exercises (approxSqrt) where
 
 -- Exercise A2
 approxSqrt' :: Double -> Double -> Double -> Double

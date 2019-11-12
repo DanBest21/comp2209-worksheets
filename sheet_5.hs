bdf2486fd9bad3d293328077a2e331fb9da97f2b
db5n17 <db5n17@soton.ac.uk>
2019-11-12 03:59:01 +0000

    Completed Exercise 2 and started work on Exercise 3 of Sheet 5.

--- sheet_5.hs
+++ sheet_5.hs
@@ -6,28 +6,31 @@ occurs x (Leaf y) = compare x y == EQ
 occurs x (Node (l) y (r)) = (compare x y) == EQ || (occurs x l) || (occurs x r)
 
 -- Exercise 2
-foldTree :: Tree a -> [a]
-foldTree (Leaf x) = [x]
-foldTree (Node (l) x (r)) = (foldTree l) ++ [x] ++ (foldTree r)
+foldTree :: (a -> b -> b) -> b -> Tree a -> b
+foldTree f v (Leaf x) = f x v
+foldTree f v (Node (l) x (r)) = f x (foldTree f (foldTree f v l) r)
 
 flatten :: Tree a -> [a]
-flatten t = foldTree t
+flatten t = foldTree (:) [] t
 
 -- Exercise 3
 data Expr = Val Int | Add Expr Expr | Sub Expr Expr
 
-foldExpr :: (Int -> Int -> Int) -> Expr -> Int
-foldExpr f (Val x) = x
-foldExpr f (Add x y) = f (foldExpr f x) (foldExpr f y) 
-foldExpr f (Sub x y) = f (foldExpr f x) (foldExpr f y)
+foldExpr :: (Int -> Int -> Int) -> Int -> Expr -> Int
+foldExpr f v (Val x) = f x v
+foldExpr f v (Add l r) = f v (foldExpr f (foldExpr f v l) r)
+foldExpr f v (Sub l r) = f v (foldExpr f (foldExpr f v l) r)
 
-eval :: Expr -> Int
-eval e = foldExpr (+) e
+-- eval :: Expr -> Int
+-- eval e = foldExpr () e
 
-size :: Expr -> Int
-size e = foldExpr (const (1+)) e
+-- size :: Expr -> Int
+-- size e = length $ foldExpr (:) [] e
 
 -- Exercise 4
+data Prop = Const Bool | Var Char | Not Prop | And Prop Prop | Imply Prop Prop
+
+
 
 -- Exercise 5
 

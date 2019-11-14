74774b105beb04456a23f1a2e4f8172e325f62fb
db5n17 <db5n17@soton.ac.uk>
2019-11-14 03:00:16 +0000

    Started work on Exercise 6.

--- sheet_5.hs
+++ sheet_5.hs
@@ -45,9 +45,36 @@ getForm (Imply n m) | p1 /= p2  = p2
                       p2 = getForm m
 
 -- Exercise 5
-data Pair a b = P (a, b)
+data Pair a b = P (a, b) deriving (Show)
+
+instance Functor (Pair a) where
+      fmap f (P (a, b)) = P (a, f b)
+
+data Fun a b = F (a -> b)
+
+instance Functor (Fun a) where
+      fmap f (F g) = F (f . g)
+
+fromFun :: Fun a b -> (a -> b)
+fromFun (F g) = g
+
+-- It would not be possible to make Fun into a functor if it was defined as F (b -> a) since fmap 
+-- is expecting a function from a -> b (?)
 
 -- Exercise 6
+data LTree a = Leaf' a | Node' (LTree a) (LTree a) 
+data Direction a = L (LTree a) | R (LTree a)
+type Trail a = [Direction a]
+type Zipper a = (LTree a, Trail a)
+
+goLeft, goRight, goUp :: (LTree a, Trail a) -> (LTree a, Trail a)
+goLeft (Node' l r, ts) = (l, L r:ts)
+goRight (Node' l r, ts) = (r, R l:ts)
+goUp (t, L r : ts) = (Node' t r, ts)
+goUp (t, R l : ts) = (Node' l t, ts)
+
+inc2LR :: LTree Int -> LTree Int
+-- inc2LR (Node' )
 
 -- Exercise 7
 
